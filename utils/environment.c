8e0471f4dd4717cbbf4b497878853f7e920c22c5
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-03-12 07:49:39 +0100

    Ajout de environment.c et implémentation de initEnvironment(...)

---
+++ utils/environment.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "environment.h"
+
+#define LENGTH 20
+
+
+
+Environment initEnvironment(Environment environment, char* identifier) {
+    if(find(identifier, environment) == NULL){
+        if((headEnv = (Environment)malloc(sizeof(struct sEnvironment))) == NULL)
+            yyerror("ERROR: Out of memory !\n");
+
+        if((headEnv->identifier = (char*)malloc(LENGTH * sizeof(struct char))) == NULL)
+            yyerror("ERROR: Out of memory !\n");
+
+        strcpy(headEnv->identifier, identifier);
+        headEnv->value = 0;
+        headEnv->next = environment;
+        return EXIT_SUCCESS;
+    }
+    return EXIT_FAILURE;
+}
+
+Environment find(char *identifier, Environment environment) {
+    return ;
+}
+
+int affect(Environment environment, char *identifier, int value) {
+    return ;
+}
+
+int printEnvironment(Environment environment) {
+    return ;
+}
+
+int getValue(Environment environment, char *identifier) {
+    return ;
+}
