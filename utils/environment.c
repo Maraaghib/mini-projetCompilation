4afc79eb7b7079bddc4ccd5e3bf585b931bac6e1
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2017-03-15 01:49:38 +0100

    Debbugage

--- utils/environment.c
+++ utils/environment.c
@@ -11,18 +11,19 @@ Environment envirAlloc() {
     return (Environment)malloc(sizeof(struct sEnvironment));
 }
 
-int initEnvironment(Environment environment, char* identifier) {
+int initEnvironment(Environment *environment, char* identifier) {
 	 Environment headEnv;
-    if(find(identifier, environment) == NULL){
+    if(find(identifier, *environment) == NULL){
         if((headEnv = envirAlloc()) == NULL)
             perror("ERROR: Out of memory !\n");
 
         headEnv->identifier = strdup(identifier);
         headEnv->value = 0;
-        headEnv->next = environment;
-        environment->identifier = strdup(identifier);
+        headEnv->next = *environment;
+        *environment = headEnv;
+        /*environment->identifier = strdup(identifier);
         environment->value = headEnv->value;
-        environment->next = NULL;
+        environment->next = NULL;*/
         return EXIT_SUCCESS;
     }
     return EXIT_FAILURE;
@@ -57,8 +58,10 @@ int affect(Environment environment, char *identifier, int value) {
 
 int printEnvironment(Environment environment) {
     Environment tmp = environment; // Est-ce ça marche !?
-    while(tmp != NULL){
-        printf("Variable: %s || Valeur: %d \n", tmp->identifier, tmp->value);
+    int cpt = 0;
+    while(tmp != NULL && tmp->identifier != NULL){
+        cpt++;
+        printf("%d) Variable: %s || Valeur: %d \n", cpt, tmp->identifier, tmp->value);
         tmp = tmp->next;
     }
     printf("End of the environment !\n");
